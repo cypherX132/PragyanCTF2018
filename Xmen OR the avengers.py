dda6bde3399699c59985ed081161412b91c38b89
cypherX132 <32241418+cypherX132@users.noreply.github.com>
2018-03-04 17:33:06 +0800

    Update and rename xor.py to Xmen OR the avengers.py

--- Xmen OR the avengers.py
+++ Xmen OR the avengers.py
@@ -1,11 +1,13 @@
 #python2
 #Made by ScriptBaby for PragyanCTF 2018
+
 import hashlib
 from Crypto.Cipher import AES
 import base64 
 
 cleartext = open("info_clear.txt","r").read()
 cipher1 = open("info_crypt.txt","r").read()
+
 key = ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(cleartext,cipher1))
 key2 = hashlib.md5(key.strip("\n")).hexdigest()
 
@@ -14,6 +16,7 @@ cipher2 = cipher2.strip("\n")
 
 obj = AES.new(key2, AES.MODE_ECB)
 cipher2 = base64.b64decode(cipher2)
+
 plain2 = obj.decrypt(cipher2)
 
 print plain2
